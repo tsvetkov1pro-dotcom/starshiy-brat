67cbfed5f2bc27608f0b7a772aaff89cdccfe85f
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-05 02:44:15 +0300

    Assemble exact Golden Master hero asset

--- src/assets/brand/hero-clean/index.ts
+++ src/assets/brand/hero-clean/index.ts
@@ -1,5 +1,10 @@
 import chunk0 from './chunk0';
 import chunk1 from './chunk1';
 import chunk2 from './chunk2';
+import chunk3 from './chunk3';
+import chunk4 from './chunk4';
+import chunk5 from './chunk5';
+import chunk6 from './chunk6';
+import chunk7 from './chunk7';
 
-export const APPROVED_HERO = `data:image/webp;base64,${chunk0}${chunk1}${chunk2}`;
+export const APPROVED_HERO = `data:image/webp;base64,${chunk0}${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}${chunk6}${chunk7}`;
