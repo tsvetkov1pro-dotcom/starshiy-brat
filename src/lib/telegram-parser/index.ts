8f81227611ad6dc672ca31701e5c28a0160f89a7
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-04 14:35:54 +0300

    feat(parser): expose Telegram parser API

---
+++ src/lib/telegram-parser/index.ts
@@ -0,0 +1,8 @@
+export { parseTelegramExport, telegramParserInternals } from './parser';
+export type {
+  ParsedTelegramMessage,
+  TelegramAttachment,
+  TelegramLink,
+  TelegramParseResult,
+  TelegramParseWarning,
+} from './types';
