968c934fc9820b0052277cd0f678b570a7544e98
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-04 17:18:24 +0300

    style: load approved brand asset crops

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/globals.css';
 import './styles/product.css';
+import './styles/brand-assets.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
