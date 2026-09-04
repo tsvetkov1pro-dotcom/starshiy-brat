f98af8b8a626759a6a53400bb6f910727a161f18
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-04 14:23:21 +0300

    feat: add normalized profile type

---
+++ src/types/profile.ts
@@ -0,0 +1,21 @@
+export interface Profile {
+  id: string;
+  telegramDisplayName: string;
+  telegramUsername?: string;
+  name?: string;
+  city?: string;
+  age?: number;
+  occupation?: string;
+  currentChallenge?: string;
+  currentPriority?: string;
+  goal90Days?: string;
+  canHelpWith?: string;
+  domains: string[];
+  challenges: string[];
+  searchKeywords: string[];
+  rawProfileText: string;
+  sourceMessageId?: string;
+  sourceDate?: string;
+  realImageReference?: string;
+  avatarSeed: string;
+}
