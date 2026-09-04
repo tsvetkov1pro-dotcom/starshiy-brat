281c467966943e1031a8d59f3c5d88d4b12a8af7
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-04 14:58:05 +0300

    feat(classification): expose classification API

---
+++ src/lib/classification/index.ts
@@ -0,0 +1,9 @@
+export {
+  CLASSIFICATION_VERSION,
+  classifyProfile,
+  classifyProfiles,
+  countTags,
+} from './classifier';
+export type { ClassificationEvidence, ClassifiedProfileResult } from './classifier';
+export { CHALLENGE_RULES, DOMAIN_RULES } from './rules';
+export type { ClassificationPattern, ClassificationRule } from './rules';
