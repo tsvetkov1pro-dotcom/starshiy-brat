bd2eb6cd6f508b55652eeb678767e05a9b69d2fb
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-05 16:06:49 +0300

    Bump Telegram parser profile semantics version

--- src/lib/telegram-parser/index.ts
+++ src/lib/telegram-parser/index.ts
@@ -1,4 +1,4 @@
-export const TELEGRAM_PARSER_VERSION = '2.0.0';
+export const TELEGRAM_PARSER_VERSION = '3.0.0';
 
 export { parseTelegramExport, telegramParserInternals } from './parser';
 export type {
