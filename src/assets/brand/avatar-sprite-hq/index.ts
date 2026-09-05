3b8f86368a8d097e286bdcdad2056bfdbfa7cbf1
tsvetkov1pro-dotcom <tsvetkov1.pro@gmail.com>
2026-09-05 15:32:52 +0300

    Assemble final HQ approved avatar sprite

---
+++ src/assets/brand/avatar-sprite-hq/index.ts
@@ -0,0 +1,9 @@
+import chunk0 from './chunk0';
+import chunk1 from './chunk1';
+import chunk2 from './chunk2';
+import chunk3 from './chunk3';
+import chunk4 from './chunk4';
+import chunk5 from './chunk5';
+
+export const APPROVED_AVATAR_SPRITE_HQ =
+  `data:image/webp;base64,${chunk0}${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}`;
